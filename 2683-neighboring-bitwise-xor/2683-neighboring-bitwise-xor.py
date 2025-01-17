e1e0cbbeb442b8984ebe505ecabc8ce60390f4f6
loktech01 <myhcl786@gmail.com>
2025-01-17 13:03:01 +0530

    Time: 162 ms (21.9%), Space: 22.4 MB (48.9%) - LeetHub

---
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.py
@@ -0,0 +1,11 @@
+class Solution:
+    def doesValidArrayExist(self, derived: List[int]) -> bool:
+        n = len(derived)
+        def is_valid(start):
+            original = [0] * n
+            original[0] = start
+            for i in range(1, n):
+                original[i] = original[i - 1] ^ derived[i - 1]       
+            return (original[n - 1] ^ original[0]) == derived[n - 1]
+
+        return is_valid(0) or is_valid(1)
